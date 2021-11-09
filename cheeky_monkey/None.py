dcaf161615fa5fe9da64f103253904f097d71e79
ezrider <michael.e.beaulieu@gmail.com>
2021-11-09 14:46:28 -0800

    setting custom menu

--- cheeky_monkey/None.py
+++ cheeky_monkey/None.py
@@ -5,3 +5,6 @@ data_file_path = r'C:\Users\mbeaulieu\rsi_project_book\Data\\'
 
 # Internal Cell
 data_file_path = r'C:\Users\mbeaulieu\rsi_project_book\Data\\'
+
+# Internal Cell
+data_file_path = r'C:\Users\mbeaulieu\rsi_project_book\Data\\'
